2fa90987f5f568fac09673bd5272bb225864e7c8
TuvaWestman <tuva.westman@hyperisland.se>
2025-11-14 11:38:20 +0100

    Create FirstTestJava.java

---
+++ FirstTestJava.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firsttestjava;
+
+/**
+ *
+ * @author tuvaw
+ */
+public class FirstTestJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hej mina skolkompisar!");
+       
+        System.out.println("Detta är en ny rad för GitHub-test!");
+    }
+    
+}
