8b2d1bbe92fcb839e5a3f3b9c46badb4755ab844
TuvaWestman <tuva.westman@hyperisland.se>
2025-11-14 11:10:27 +0100

    Första commit: lägger till Java-projekt

---
+++ src/main/java/com/mycompany/firsttestjava/FirstTestJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firsttestjava;
+
+/**
+ *
+ * @author tuvaw
+ */
+public class FirstTestJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hej mina skolkompisar!");
+    }
+}
