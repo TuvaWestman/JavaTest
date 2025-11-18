3d922b4ac67064231091c348c69846e8a260976c
MJ-student439 <mjohansson@live.se>
2025-11-18 11:30:35 +0100

    Update FirstTestJava.java
    test

--- FirstTestJava.java
+++ FirstTestJava.java
@@ -4,19 +4,39 @@
 
 package com.mycompany.firsttestjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tuvaw
  */
 public class FirstTestJava {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hej mina skolkompisar!");
-       
-        System.out.println("Detta är en ny rad för GitHub-test!");
-        System.out.println("Martina här");
-        
-        
+        // TODO code application logic here
+        System.out.println("welcome");
     }
     
+    
+package projectdungeon;
+
+
+
+
+import java.util.Scanner;
+public class Player {
+//This is the player class 
+public static void main (String[] args) {
+Scanner input= new Scanner(System.in); 
+String name ;
+System.out.print("Enter your name: ");
+name = input.nextLine(); 
+input.close();
 }
+}
+        
+    
+  
