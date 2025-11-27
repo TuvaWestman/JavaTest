8c8d4dcd233cef35a452801505a10f2dc23e6475
Carlos-Ars <carlosarsalan03@gmail.com>
2025-11-27 21:05:20 +0100

    Update Door.java
    Skapade Door (klass objekt) med constructor och getters samt setters (setters bara för att få in det i uppgiften). La lite random namn och anteckningar lite här och där, så ni får gärna ändra eller skriva om något !

--- java/Door.java
+++ java/Door.java
@@ -17,5 +17,35 @@ isLocked()bool  - public (this is a method)
 */
 
 public class Door {
-    
+
+    private String Riktning;
+    private Room lederTill;
+
+
+    // konstuktor (skapar objekt av klassen & sätter startvärde)
+    public Door(String Riktning, Room lederTill) {
+        this.Riktning = Riktning;
+        this.lederTill = lederTill;
+    }
+
+
+    //kan finnas setters här, lägg till// setters.. (anteckning ta bort sen)
+    public void setRiktning(String Riktning){
+        this.Riktning = Riktning;
+    }
+
+    // getters för Door (attribut)
+    public String getRiktning(){
+        return Riktning;
+    }
+
+
+    public void setLederTill(){
+        this.lederTill = lederTill;
+    }
+
+    public Room getLederTill(){
+        return lederTill;
+    }
+
 }
