dfed4f96c34a52e7b8ee572a39df4604771e46b6
Carlos-Ars <carlosarsalan03@gmail.com>
2025-12-01 20:15:45 +0100

    Update Room.java
    La till Room objekt med en kort for loop, kontruktor, getters och setters. Namnen bör nog anpassas beroende på vilka namn vi väljer att ha sen, om vi ska skriva det på engelska osv...

--- java/Room.java
+++ java/Room.java
@@ -12,6 +12,49 @@
 roomDesc: Sting - private (class attribute, instance variable)
 do Narritive():void - public (method, class operator)
 */
+
+
+
 public class Room {
-    
+
+    private String Beskrivning;
+    private Door [] DoorRiktning;   // skapar arrays för olika dörrar som går olika riktningar
+
+    // konstruktor
+    public Room (String Beskrivning, Door[] DoorRiktning){
+        this.Beskrivning = Beskrivning;
+        this.DoorRiktning = DoorRiktning;
+    }
+
+    //skriver ut beskrivningen utan att behöva ändra värden här
+    public void visaBeskrivning() {
+        System.out.println(Beskrivning);
+    }
+
+
+    public void visaDoor() {
+        for(Door d : DoorRiktning){
+            System.out.println("Du kan gå " + d.getRiktning());
+        } //metod. Kopplar till Riktning i Doors för att hämta n, s, ö, v
+        // d är alias från klassen Door där getRiktning kommer ifrån
+    }
+
+
+    public String getBeskrivning(){
+        return Beskrivning;
+    }
+
+    public void setBeskrivning(String Beskrivning){
+        this.Beskrivning = Beskrivning;
+    }
+
+
+    public void setDoorRiktning(Door[] DoorRiktning){
+        this.DoorRiktning = DoorRiktning;
+    }
+
+    public Door[] getDoorRiktning(){
+        return DoorRiktning;
+    }
+
 }
