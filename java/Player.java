24e8babeaf86af0fdd3cf29a64bdbfbe750e1d12
Carlos-Ars <carlosarsalan03@gmail.com>
2025-12-02 10:03:44 +0100

    Update Player.java

--- java/Player.java
+++ java/Player.java
@@ -20,8 +20,10 @@ public class Player {
 
 
     public void setName(String name){
-        this name = name; }
+        this.name = name; }
     public String getName(){
         return name;
     }
-    }
+
+
+}
